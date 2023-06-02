882ad4f7b9a4d369565254e8d65603d257b3f25b
ErickFreitasCosta <erickifpa2021@gmail.com>
2023-06-02 11:51:59 -0300

    tirei o radius

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -5,7 +5,7 @@ function Footer() {
     <nav className="navbar fixed-bottom navbar-dark bg-dark">
       <div className="container-fluid footer-p" id="footer-p">
         <a className="navbar-brand" href="#" id="footerLetra">
-          <i> &copy; Direitos reservados - By Erick</i>
+          <i>  <p className="footer-nome"> &copy; Direitos reservados - By Erick</p></i>
         </a>
       </div>
     </nav>
